67d8661d95f0918fc2cff0cb2d500f68c1d9565f
Nate Landon <druidprogramming@gmail.com>
2018-03-22 12:18:21 -0600

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
